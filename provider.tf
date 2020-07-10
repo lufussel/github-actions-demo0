76d04e963432969ae805271a694967b07ffcd4ec
Luke Fussell <44374451+lufussel@users.noreply.github.com>
2020-07-10 16:34:54 +0100

    Corrected provider block

--- provider.tf
+++ provider.tf
@@ -1,8 +1,10 @@
 terraform {
-  required_version     = ">= 0.12"
-  storage_account_name = "ghademo0tfstate"
-  container_name       = "tfstate"
-  key                  = "${var.env}.terraform.tfstate"
+  required_version = ">= 0.12"
+  backend "azurerm" {
+    storage_account_name = "ghademo0tfstate"
+    container_name       = "tfstate"
+    key                  = "${var.env}.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
