d055c66b1b30a9baff12958d2a68fddcc9b4af99
Luke Fussell <luke.fussell@microsoft.com>
2020-07-10 15:54:23 +0100

    Added basic terraform

---
+++ provider.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version     = ">= 0.12"
+  storage_account_name = "ghademo0tfstate"
+  container_name       = "tfstate"
+  key                  = "${var.env}.terraform.tfstate"
+}
+
+provider "azurerm" {
+  version = "= 2.0.0"
+  features {}
+}
