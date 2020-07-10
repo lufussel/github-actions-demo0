b9e851763c8962fc08310362ddfdf5f43440e1e2
Luke Fussell <44374451+lufussel@users.noreply.github.com>
2020-07-10 16:46:30 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,12 @@
 terraform {
   required_version = ">= 0.12"
+  
   backend "azurerm" {
     storage_account_name = "ghademo0tfstate"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
-}
+}f
 
 provider "azurerm" {
   version = "= 2.0.0"
