4255e6fab715cd83abd4162b45b1450405ddd87f
Luke Fussell <44374451+lufussel@users.noreply.github.com>
2020-07-14 15:50:58 +0100

    Added storage account

--- main.tf
+++ main.tf
@@ -14,3 +14,14 @@ resource "azurerm_storage_account" "storage_account" {
 
   tags = var.tags
 }
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "${var.env}${var.app_name}2"
+  resource_group_name      = azurerm_resource_group.storage_rg.name
+  location                 = azurerm_resource_group.storage_rg.location
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = var.tags
+}
